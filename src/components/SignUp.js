7940710ac6dcde71d71f728b68608db3905c0d7d
Muniba <mbashirkimes@gmail.com>
2023-07-31 15:54:06 -0400

    Update environment variables

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -27,36 +27,36 @@ export default function SignUp() {
       setError('Failed to create an account');
     }
     setLoading(false);
-  }
+    }
 
-  return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Sign Up</h2>
-          {currentUser && currentUser.email}
-          {error && <Alert variant="danger">{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">Already have an account? Log In</div>
-    </>
-  );
-}
+    return (
+        <>
+        <Card>
+            <Card.Body>
+            <h2 className="text-center mb-4">Sign Up</h2>
+            {currentUser.email}
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+                <Form.Group id="password-confirm">
+                <Form.Label>Password Confirmation</Form.Label>
+                <Form.Control type="password" ref={passwordConfirmRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                Sign Up
+                </Button>
+            </Form>
+            </Card.Body>
+        </Card>
+        <div className="w-100 text-center mt-2">Already have an account? Log In</div>
+        </>
+    );
+    }
 
