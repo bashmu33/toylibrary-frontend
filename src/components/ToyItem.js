e46daf609b76e6140fc8414519c5c8b48503d852
Muniba <mbashirkimes@gmail.com>
2023-08-04 21:38:53 -0400

    fixes the toyitem component to render the reserve button for logged in users and delete and check out buttons for admin

--- src/components/ToyItem.js
+++ src/components/ToyItem.js
@@ -1,92 +1,5 @@
-// import { Card, Button } from "react-bootstrap";
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { useAuth } from '../contexts/AuthContext';
-// import axios from "axios";
-
-// const kBaseUrl = "https://toy-library.onrender.com"
-
-// const ToyItem = ({ toy, setToys }) => {
-//   const { toy_id, toy_image, toy_name, description, age_category, toy_status } = toy;
-//   const auth = useAuth();
-
-//   const handleReserveButtonClick = async (toy_id) => {
-//     try {
-//       const user = auth.currentUser;
-//       const userId = user.uid;
-  
-//       const response = await axios.post(
-//         `${kBaseUrl}/users/${userId}/reserve/${toy_id}`
-//       );
-  
-//       if (response.status === 200) {
-//         // Update the reservation status in the UI
-//         setToys((prevToys) => {
-//           console.log('prevToys before update:', prevToys);
-
-//           const updatedToys = prevToys.map((toy) =>
-//             toy.toy_id === toy_id
-//               ? { ...toy, toy_status: 'reserved' }
-//               : toy
-//           );
-
-//           console.log('updatedToys:', updatedToys);
-
-//           return updatedToys;
-//         });
-//       }
-//     } catch (error) {
-//       console.error('Error reserving toy:', error);
-//     }
-//   };
-  
-
-//   return (
-//     <Card style={{ position: 'relative', marginBottom: '20px', width: '45%', minWidth: '300px' }}>
-//       <div style={{ display: 'flex', height: '100%' }}>
-//         <div style={{ width: '40%', padding: '10px' }}>
-//           <img
-//             src={toy_image}
-//             alt={toy_name}
-//             style={{ width: '100%', height: '250px', objectFit: 'cover' }}
-//           />
-//         </div>
-//         <div style={{ width: '60%', padding: '10px', position: 'relative' }}>
-//           <Card.Body>
-//             <Card.Title>{toy_name}</Card.Title>
-//             <Card.Text>Recommended Age:{age_category}</Card.Text>
-//             <Card.Text>{description}</Card.Text>
-//           </Card.Body>
-//           <Button
-//               style={{ position: 'absolute', bottom: '10px', right: '10px' }}
-//               variant={toy_status === 'available' ? 'primary' : 'secondary'}
-//               disabled={toy_status === 'reserved'}
-//               onClick={() => handleReserveButtonClick(toy_id)}
-//             >
-//               {toy_status === 'available' ? 'Reserve Toy' : 'Reserved'}
-//             </Button>
-//         </div>
-//       </div>
-//     </Card>
-//   );
-// };
-
-// ToyItem.propTypes = {
-//   toy: PropTypes.shape({
-//     toy_id: PropTypes.number.isRequired,
-//     toy_name: PropTypes.string.isRequired,
-//     description: PropTypes.string,
-//     age_category: PropTypes.string,
-//     toy_status: PropTypes.string.isRequired,
-//     toy_image: PropTypes.string, 
-//   }).isRequired,
-//   setToys: PropTypes.func.isRequired, 
-// };
-
-// export default ToyItem;
-
 import { Card, Button } from 'react-bootstrap';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useAuth } from '../contexts/AuthContext';
 import axios from 'axios';
@@ -96,29 +9,43 @@ const kBaseUrl = 'https://toy-library.onrender.com';
 const ToyItem = ({ toy, setToys }) => {
   const { toy_id, toy_image, toy_name, description, age_category, toy_status } = toy;
   const auth = useAuth();
+  const [isAdminUser, setIsAdminUser] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    const handleReserveButtonClick = async (toy_id) => {
+  useEffect(() => {
+    // Check if the current user is admin
+    const checkAdminStatus = async () => {
+      const adminStatus = await auth.isAdmin();
+      setIsAdminUser(adminStatus);
+    };
+
+    // Check if a user is logged in
+    const checkLoggedInStatus = () => {
+      const user = auth.currentUser;
+      setIsLoggedIn(user !== null);
+    };
+
+    checkAdminStatus();
+    checkLoggedInStatus();
+  }, []);
+
+  const handleReserveButtonClick = async (toy_id) => {
     try {
       const user = auth.currentUser;
       const userId = user.uid;
-  
+
       const response = await axios.post(
         `${kBaseUrl}/users/${userId}/reserve/${toy_id}`
       );
-  
+
       if (response.status === 200) {
         // Update the reservation status in the UI
         setToys((prevToys) => {
-          console.log('prevToys before update:', prevToys);
-
           const updatedToys = prevToys.map((toy) =>
             toy.toy_id === toy_id
               ? { ...toy, toy_status: 'reserved' }
               : toy
           );
-
-          console.log('updatedToys:', updatedToys);
-
           return updatedToys;
         });
       }
@@ -129,7 +56,7 @@ const ToyItem = ({ toy, setToys }) => {
 
   const handleCheckOutButtonClick = async (toy_id) => {
     try {
-      // ... existing checkout logic ...
+      // Handle check out logic here
     } catch (error) {
       console.error('Error checking out toy:', error);
     }
@@ -137,14 +64,17 @@ const ToyItem = ({ toy, setToys }) => {
 
   const handleDeleteButtonClick = async (toy_id) => {
     try {
-      // ... existing delete logic ...
+      const confirmDelete = window.confirm('Are you sure you want to delete this toy?');
+      if (!confirmDelete) return;
+
+      await axios.delete(`${kBaseUrl}/toys/${toy_id}/confirm_delete`);
+
+      // Remove the deleted toy from the list
+      setToys((prevToys) => prevToys.filter((t) => t.toy_id !== toy_id));
     } catch (error) {
       console.error('Error deleting toy:', error);
     }
   };
-
-  const isAdmin = auth.isAdmin();
-  const isLoggedIn = auth.currentUser !== null;
 
   return (
     <Card style={{ position: 'relative', marginBottom: '20px', width: '45%', minWidth: '300px' }}>
@@ -162,7 +92,7 @@ const ToyItem = ({ toy, setToys }) => {
             <Card.Text>Recommended Age: {age_category}</Card.Text>
             <Card.Text>{description}</Card.Text>
           </Card.Body>
-          {isAdmin && isLoggedIn && (
+          {isAdminUser && (
             <>
               <Button
                 style={{ position: 'absolute', bottom: '10px', left: '10px' }}
@@ -181,7 +111,7 @@ const ToyItem = ({ toy, setToys }) => {
               </Button>
             </>
           )}
-          {!isAdmin && (
+          {!isAdminUser && isLoggedIn && (
             <Button
               style={{ position: 'absolute', bottom: '10px', right: '10px' }}
               variant={toy_status === 'available' ? 'primary' : 'secondary'}
@@ -210,5 +140,3 @@ ToyItem.propTypes = {
 };
 
 export default ToyItem;
-
-
